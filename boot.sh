54aa58f7f656dc73074db0b833ce6593a92bb59f
Jacob <jgoldverg@gmail.com>
2020-06-16 17:40:41 -0400

    Set timer for Eviction Policy

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
-export EUREKA_USER=eureka
-export EUREKA_PASS=eureka
+export EUREKA_USER=admin
+export EUREKA_PASS=admin
 nohup java -jar ods-discovery-service-0.0.1-SNAPSHOT.jar -Xms 500m -Xmx 800m -XX:+UseG1GC &
