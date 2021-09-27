63e3488fc6055615c476db2b140d74598f1a974c
Jacob Goldverg <jgoldverg@gmail.com>
2021-09-27 16:10:25 -0400

    updated to Java 11 and updated the version as well

--- src/main/java/org/onedatashare/odsdiscoveryservice/AdminSecurity.java
+++ src/main/java/org/onedatashare/odsdiscoveryservice/AdminSecurity.java
@@ -1,16 +1,8 @@
 package org.onedatashare.odsdiscoveryservice;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.UUID;
 
 @EnableWebSecurity
 class AdminSecurity extends WebSecurityConfigurerAdapter {
